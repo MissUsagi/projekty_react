f7b9c9934533e93c3c777a4315c9d99aa2f51dfd
MissUsagi <syljanko@gmail.com>
2023-03-24 18:04:06 +0100

    adding new posts functionality

--- react-pin-app/src/components/NewPost.jsx
+++ react-pin-app/src/components/NewPost.jsx
@@ -1,18 +1,56 @@
-import classes from './NewPost.module.css';
+import classes from "./NewPost.module.css";
+import { useState } from "react";
 
-function NewPost(props) {
-   return (
-      <form className={classes.form}>
-         <p>
-            <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} onChange={props.onMessageChange} />
-         </p>
-         <p>
-            <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required onChange={props.onNicknameChange} />
-         </p>
-      </form>
-   )
+function NewPost({ onCancelForm, onAddPost }) {
+  const [enteredMessage, setEnteredMessage] = useState("");
+  const [enteredNickname, setEnteredNickname] = useState("");
+
+  function messageChangeHandler(event) {
+    setEnteredMessage(event.target.value);
+  }
+
+  function nicknameChangeHandler(event) {
+    setEnteredNickname(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      message: enteredMessage,
+      author: enteredNickname,
+    };
+    onAddPost(postData);
+    onCancelForm();
+  }
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <p>
+        <label htmlFor="message">Text</label>
+        <textarea
+          id="message"
+          required
+          rows={3}
+          onChange={messageChangeHandler}
+        />
+      </p>
+      <p>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          required
+          onChange={nicknameChangeHandler}
+        />
+      </p>
+      <div className={classes.actions}>
+        <button type="button" onClick={onCancelForm}>
+          Cancel
+        </button>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
 }
 
 export default NewPost;
--- react-pin-app/src/components/PostsList.jsx
+++ react-pin-app/src/components/PostsList.jsx
@@ -1,38 +1,41 @@
-import { useState } from 'react';
-import NewPost from './NewPost';
-import Post from './Post';
-import Modal from './Modal';
-import classes from './PostsList.module.css';
+import NewPost from "./NewPost";
+import Post from "./Post";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+import { useState } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
-   const [enteredMessage, setEnteredMessage] = useState('');
-   const [enteredNickname, setEnteredNickname] = useState('');
+  const [posts, setPosts] = useState([]);
 
-   function messageChangeHandler(event) {
-      setEnteredMessage(event.target.value)
-   }
-
-   function nicknameChangeHandler(event) {
-      setEnteredNickname(event.target.value)
-   }
-
-   return (
-      <>
-         {isPosting ?
-            <Modal onCloseModal={onStopPosting}>
-               <NewPost
-                  onMessageChange={messageChangeHandler}
-                  onNicknameChange={nicknameChangeHandler}
-               />
-            </Modal>
-            : null
-         }
-         <ul className={classes.posts}>
-            <Post username={enteredNickname} message={enteredMessage} />
-            <Post username="test" message="fssss" />
-         </ul>
-      </>
-   )
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]); //nowy post jako 1szy
+  }
+  return (
+    <>
+      {isPosting ? (
+        <Modal onCloseModal={onStopPosting}>
+          <NewPost onCancelForm={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      ) : null}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              key={post.message}
+              username={post.author}
+              message={post.message}
+            /> //change key to unique id
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default PostList;
